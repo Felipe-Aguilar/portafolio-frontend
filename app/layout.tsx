e2150ac3acf14a8cb8eaeb56213cea621a30d4b3
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2023-12-18 19:24:10 -0600

    Instalando Next Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { DM_Sans } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
-import GoogleAnalytics from './GoogleAnalytics';
+import GoogleAnalytics from '@bradgarropy/next-google-analytics';
 
 const inter = DM_Sans({ subsets: ['latin'] })
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="es">
       <body className={inter.className}>
         <Header />
-        <GoogleAnalytics />
+        <GoogleAnalytics measurementId='G-7KJJHHM3R9'/>
         {children}
         <Footer />
       </body>
